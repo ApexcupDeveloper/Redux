08ad98385c86159945a72f4c6fc441a5ba345000
Admin <admin@Admins-Mac-6.local>
2020-03-09 18:43:44 +0800

    react redux example

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+import store from './redux/store'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
